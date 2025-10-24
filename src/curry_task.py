95a4978924d452cb143ab26da8dc6548b6b892b9
Yana Kalsina <yaprogrammer18@gmail.com>
2025-10-24 21:39:39 +0300

    Finally finished working with tests and reated final version of curry function

--- src/curry_task.py
+++ src/curry_task.py
@@ -8,6 +8,8 @@ def curry(func, arity):
         raise MyError("Арность не может быть меньше или равна 0.")
     if arity > func.__code__.co_argcount:
         raise MyError("Переданная арность больше действительной.")
+    if not isinstance(arity, int):
+        raise MyError("Переданная арность функции обязана быть целым числом.")
     lst = []
 
     def inner(arg):
@@ -19,7 +21,6 @@ def curry(func, arity):
             lst = []
             return tmp
         return inner
-
     return inner
 
 
@@ -40,6 +41,3 @@ def uncurry(curried_func, arity):
         return tmp_func
 
     return uncarried_func
-
-
-# P.S. У меня такое ощущение, что за подобный код мне могут дать по рукам, хоть оно и работает, но зато сама:DDD
