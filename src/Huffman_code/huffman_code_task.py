72131217aff96eb15cf528b5d9ffc0a5b2ab0ed4
Yana Kalsina <yaprogrammer18@gmail.com>
2025-11-16 11:39:33 +0300

    Finish encode func

--- src/Huffman_code/huffman_code_task.py
+++ src/Huffman_code/huffman_code_task.py
@@ -1,68 +1,58 @@
-import heapq
-
-
 class Node:
     def __init__(self, symbol=None, freq=0):
         self.symbol = symbol
         self.freq = freq
         self.left = None
         self.right = None
-        self.path = None
+        self.path = ''
 
-    def __lt__(self, other):
-        return self.freq < other.freq
 
-# разбить на мелкие функции
 def encode(msg: str) -> tuple[str, dict[str, str]]:
-
-    msg = list(msg)
     lst_of_nodes = []
-    while msg != []:
-        node = Node(msg[0], msg.count(msg[0]))
-        msg = (''.join(msg)).replace(msg[0], '')
+
+    def make_table_symbol_and_freq(msg):
         msg = list(msg)
-        lst_of_nodes.append(node)
+        while msg != []:
+            node = Node(msg[0], msg.count(msg[0]))
+            msg = (''.join(msg)).replace(msg[0], '')
+            msg = list(msg)
+            lst_of_nodes.append(node)
 
-    while len(lst_of_nodes) > 1:
-        lst_of_nodes.sort(key=lambda x: x.freq)
-        left = lst_of_nodes.pop(0)
-        right = lst_of_nodes.pop(0)
-        # создаем новый элемент с частотой равной сумме двух выбранных
-        node = Node(freq=left.freq+right.freq)
-        node.left = left
-        node.right = right
-        lst_of_nodes.append(node)
+    def heapify(lst_of_nodes):
+        while len(lst_of_nodes) > 1:
+            lst_of_nodes.sort(key=lambda x: x.freq)
+            left = lst_of_nodes.pop(0)
+            right = lst_of_nodes.pop(0)
 
+            # создаем новый элемент с частотой равной сумме двух выбранных
+            node = Node(freq=left.freq + right.freq)
+            node.left = left
+            node.right = right
+            lst_of_nodes.append(node)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # отсортировать
-    # цикл с сортировкой + объединением + добавлением пути и тп
-
-
-
-encode('122333444455555')
+    make_table_symbol_and_freq(msg)
+    heapify(lst_of_nodes)
+    table = {}
+    def code_and_collect_every_path(node, prefix):
+        if node.symbol is not None:
+            node.path = prefix
+            table[node.symbol] = node.path
+        else:
+            if node.left:
+                code_and_collect_every_path(node.left, prefix + "0")
+            if node.right:
+                code_and_collect_every_path(node.right, prefix + "1")
 
 
 
+    if lst_of_nodes:
+        code_and_collect_every_path(lst_of_nodes[0], '')
 
+    encoded_msg = ''
+    for el in msg:
+        encoded_msg += table[el]
 
-
-
-
-
-
+    return (encoded_msg, table)
 
 
 
