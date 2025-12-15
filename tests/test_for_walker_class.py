e78ec5876a19a4b393d9a16a1bf772961a7f75bf
Yana Kalsina <yaprogrammer18@gmail.com>
2025-12-15 15:26:26 +0300

    Remove one test

--- tests/test_for_walker_class.py
+++ tests/test_for_walker_class.py
@@ -12,11 +12,6 @@ def test_error():
         Walker([["A", 0], ["D", 0]])
 
 
-def test_usual_occasion():
-    a = Walker([["A", 0.24], ["D", 0.11], ["E", 0.34], ["C", 0.28], ["B", 0.03]])
-    print(a.events_and_prob)
-
-
 def test_empty_list():
     with pytest.raises(ValueError, match="Переданный список пустой"):
         Walker([])
